812a9da057b9667d8618f3969556a030f0a8cbc8
Garrett Snedeker <garrettsnedeker20@gmail.com>
2023-03-02 17:42:17 -0600

    added imports

--- login-function/index.js
+++ login-function/index.js
@@ -1,73 +1,65 @@
-const jwt = require('jsonwebtoken');
+const loginDAO = require('./login-dao')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const LoginError = require('./login-errors')
 
-class LoginError extends Error {
-    constructor(errors) {
-      super("Login Error");
-      this.errors = errors;
-    }
-  }
 
-exports.handler = async (event) => {
-   
+exports.handler = async (event, context) => {
+    const path = event.pathParameters;
+    const body = JSON.parse(event.body);
+
     try {
         const username = event.body.username;
         const password = event.body.password;
 
-    
         if (!username || !password) {
-          throw new Error('Username and/or password not provided');
+            throw new Error('Username and/or password not provided');
         }
-    
-        const token = await login(username, password);
-    
-        return res.send({
-          "message": "Login successful",
-          "token": token
-        });
-      } catch(err) {
-        if (err instanceof LoginError) {
-          return res.status(400).send({
-            "errors": err.errors
-          })
-        }
-    
-        return res.status(500).send({
-          "errors": [ err.message ]
-        });
-      }
-   
 
+        const token = await login(username, password);
+
+        return res.send({
+            "message": "Login successful",
+            "token": token
+        });
+    } catch (err) {
+        if (err instanceof LoginError) {
+            return res.status(400).send({
+                "errors": err.errors
+            })
+        }
+
+        return res.status(500).send({
+            "errors": [err.message]
+        });
+    }
 }
 
+
+//Login function
 async function login(username, password) {
-    const data = await userDao.retrieveUserByUsername(username);
-  
+    const data = await loginDAO.retrieveUserByUsername(username);
+
     const errors = [];
     if (!data.Item) {
-      errors.push("Invalid username");
+        errors.push("Invalid username");
     } else if (!(await bcrypt.compare(password, data.Item.password))) {
-      errors.push("Invalid password");
+        errors.push("Invalid password");
     }
-  
+
     if (errors.length > 0) {
-      throw new LoginError(errors);
+        throw new LoginError(errors);
     }
-  
-    return jwtUtil.createToken(data.Item.username, data.Item.role);
-  }
 
+    return createToken(data.Item.username, data.Item.role);
+}
 
-
-  function createToken(username, role) {
+//create JWT upon login
+function createToken(username, role) {
     return jwt.sign({
-      username: username,
-      role: role
+        username: username,
+        role: role
     }, process.env.JWT_SIGNING_SECRET)
-  }
+}
 
-
-
-
-
-module.exports= {retrieveUserByUsername,login}
+module.exports = {login};
