55883de2d419f2f1f17d60c1f3e2958aababce04
Christopher C <83333157+ChristopherC360@users.noreply.github.com>
2023-03-09 10:56:08 -0600

    Update index.js

--- add-reimbursements-function/index.js
+++ add-reimbursements-function/index.js
@@ -9,7 +9,7 @@ exports.handler = async (event) => {
     const bodyObject = JSON.parse(event.body);
 
     try {
-      //const payload = await authService.authorizeEmployee(event.headers.authorization);
+      const payload = await authService.authorizeEmployee(event.headers.authorization);
   
       await reimbService.addReimbursement(bodyObject);
   
